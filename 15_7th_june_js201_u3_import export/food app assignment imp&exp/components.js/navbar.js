f89ccaac395c35a3730c2199c620ef25f112430a
SpRevision <106931382+SpRevision@users.noreply.github.com>
2022-06-12 00:26:53 +0530

    Add files via upload

---
+++ 15_7th_june_js201_u3_import export/food app assignment imp&exp/components.js/navbar.js
@@ -0,0 +1,14 @@
+let navbar=()=>{
+return ` <h3><a href="index.html">Home</a></h3>
+
+<div id="options">
+<h3><a href="recipe.html">Recipe of the day</a></h3>
+    <h3><a href="login.html">LogIn</a></h3>
+    <h3  id="signup"><a href="signup.html">SignUp</a></h3>
+
+</div>
+
+`;
+}
+
+export default navbar()
---
+++ 15_7th_june_js201_u3_import export/we using import export/copo/navbar.js
@@ -0,0 +1,14 @@
+function navbar(){
+    return `
+    <h3><a href="index.html">Home</a></h3>
+    <div id="options">
+        <h3><a href="electronics.html">Electronics</a></h3>
+        <h3><a href="jewelery.html">Jewelery</a></h3>
+        <h3>login</h3>
+        <h3>signup</h3>
+    </div>
+    `;
+
+}
+
+export default navbar()
