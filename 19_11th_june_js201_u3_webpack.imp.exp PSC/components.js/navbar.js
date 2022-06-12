80154cc093f656c34663890c61592871fa47db93
SpRevision <106931382+SpRevision@users.noreply.github.com>
2022-06-13 02:28:32 +0530

    imageApp,imp,exp,sort,login,signup,debounce

---
+++ 19_11th_june_js201_u3_webpack.imp.exp PSC/components.js/navbar.js
@@ -0,0 +1,15 @@
+let navbar=()=>{
+return ` <h3><a href="index.html">Home</a></h3>
+<h2>Explore Images ☺</h2>
+<div id="options">
+<h3><a href="index.html">Search Page </a></h3>
+<h3><a href="recipe.html">Categories Page</a></h3>
+    <h3><a href="login.html">LogIn</a></h3>
+    <h3  id="signup"><a href="signup.html">SignUp</a></h3>
+
+</div>
+
+`;
+}
+
+export default navbar()
